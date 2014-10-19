5f2bfe8274ed346b546c45da200ee802ff1c88f7
tankovski <tankovski.stoian@gmail.com>
2014-10-19 14:20:49 +0300

    loading img bug fixed

--- ShouldIEatThatFood/app/view-models/home-view-model.js
+++ ShouldIEatThatFood/app/view-models/home-view-model.js
@@ -10,8 +10,6 @@
     function takePhoto() {
         var that = this;
 
-        global.app.application.showLoading();
-
         window.cameraApp.run();
         window.cameraApp.capturePhoto().then(function (fileObj) {
 
@@ -21,33 +19,29 @@
                 { Authorization: "Bearer " + accessToken })
                 .then(function (data) {
                     // sucsess 
-                    kendo.mobile.application.hideLoading();
                     showResults(data.allowed, data.warning, data.dangerous);
                     that.set("isImageVisible", true);
                     that.set("imageSrc", "data:image/png;base64, " + fileObj);
                     saveUploadImage(fileObj, window.submitStatus.succses);
 
                 }, function (error) {
-                    kendo.mobile.application.hideLoading();
+
                     alert("Uplouding is not successful, image will be saved in history for later.");
                     saveUploadImage(fileObj, window.submitStatus.error);
                    
                });
-
+        }, function (error) {
         });
 
     }
     
         function saveUploadImage(imageString, status) {
             var savedImages = localStorage.getObject(window.pendingTags);
-            if (!savedImages) {
-                savedImages = [];
-            }
-            savedImages.unshift({ image: imageString, status: status });
+            savedImages.unshift(fileObj);
             if (savedImages.lenght > window.pendingTagsMaxCount) {
                 savedImages.pop();
             }
-            localStorage.setObject(window.pendingTags, savedImages);
+            localStorage.setObject(window.pendingTags, { image: fileObj , status: status});
         }
 
         function showResults(allowed, warning, dangerous) {
